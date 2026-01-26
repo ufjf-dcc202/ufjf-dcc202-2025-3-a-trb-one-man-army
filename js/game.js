09043fc160c59e0c61029569be739918c722696f
Matheus Estevao Faria <estevaox@DESKTOP-ISEDIV3.localdomain>
2026-01-25 22:36:24 -0300

    feat: renderização por grid e atualização do carregamento de tiles, mudança no titulo e favicon

--- js/game.js
+++ js/game.js
@@ -1,5 +1,21 @@
 const TILE_WIDTH = 100;
 const TILE_HEIGHT = 50;
+const GRID_SIZE = 10;
+const MAX_TILE_HEIGHT = 80;
+
+let grid = [
+  [14, 23, 23, 23, 23, 23, 23, 23, 23, 13],
+  [21, 32, 33, 33, 28, 33, 28, 33, 31, 20],
+  [21, 34,  0,  0, 25, 33, 30,  1, 34, 20],
+  [21, 34,  0,  0, 34,  1,  1, 10, 34, 20],
+  [21, 25, 33, 33, 24, 33, 33, 33, 27, 20],
+  [21, 34,  4,  7, 34, 18, 17, 10, 34, 20],
+  [21, 34,  4,  7, 34, 16, 19, 10, 34, 20],
+  [21, 34,  6,  8, 34, 10, 10, 10, 34, 20],
+  [21, 29, 33, 33, 26, 33, 33, 33, 30, 20],
+  [11, 22, 22, 22, 22, 22, 22, 22, 22, 12]
+];
+
 
 let tile_images = []
 let x_start ;
@@ -8,7 +24,9 @@ let y_start ;
 function draw_tile(img, x, y) {
   let x_screen = x_start + (x - y) * TILE_WIDTH/2;
   let y_screen = y_start + (x + y) * TILE_HEIGHT/2;
-  image(img, x_screen,y_screen);
+  let z_offset = MAX_TILE_HEIGHT - img.height;
+  
+  image(img, x_screen, y_screen + z_offset);
 }
 
 
@@ -16,20 +34,20 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     x_start = width/2 - TILE_WIDTH/2; 
     y_start = 50;
-    tile_images.push(loadImage('./tiles/grass.png'));
-    tile_images.push(loadImage("./tiles/sand.png"));
-    tile_images.push(loadImage("./tiles/water.png"));
+    for (let i = 0; i <= 34; i++) {
+    tile_images.push(loadImage("./tiles/tile-" + i + ".png"));
+  }
+}
+
+function draw_grid() {
+  for (let i = 0; i < GRID_SIZE; i++) {
+    for (let j = 0; j < GRID_SIZE; j++) {
+      draw_tile(tile_images[grid[j][i]], i, j);
+    }
+  }
 }
 
 function draw() {
   background("black");
-  draw_tile(tile_images[0], 0, 0);
-  draw_tile(tile_images[1], 1, 0);
-  draw_tile(tile_images[0], 2, 0);
-  draw_tile(tile_images[0], 0, 1);
-  draw_tile(tile_images[1], 1, 1);
-  draw_tile(tile_images[0], 2, 1);
-  draw_tile(tile_images[0], 0, 2);
-  draw_tile(tile_images[1], 1, 2);
-  draw_tile(tile_images[0], 2, 2);
+  draw_grid();
 }
